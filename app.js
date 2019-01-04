c6e42f84e9e7db5bf3b5af9c6370a663cd56f3e3
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-01-04 15:20:02 +0000

    updating app.js

--- app.js
+++ app.js
@@ -14,8 +14,27 @@ re = /gre?a?y/i; // Optional Character
 re = /gre?a?y\?/i; // Escape Character
 
 
+// Brackets [] - Characters Sets
+re = /gr[ae]/i; // Must be an a or e
+re = /[GF]ray/i; // Must be an G or F
+re = /[^GF]ray/i; // Match anything except a G or F
+re = /[A-Z]ray/; // Match any uppercase letter
+re = /[a-z]ray/; // Match any lowercase letter
+re = /[A-Za-z]ray/; // Match any letter
+re = /[0-9]ray/; // Match any digit
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i; // Must occur exactly {m} amount of times
+re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times
+re = /Hel{2,}o/i; // Must occur at least {m} times
+
+// Parethesis () - Grouping
+re = /^([0-9]x){3}$/ // Looking at the begining and the end
+
+
+
 // String to match
-const str = 'Hello World';
+const str = '3x3x3x';
 
 // Log results
 const result = re.exec(str);
