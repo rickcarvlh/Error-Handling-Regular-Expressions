e5cc8c3f836943c4f14a83b9867736b7ea304b4a
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-01-04 16:00:45 +0000

    updating app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let re;
 
-// Literal Characters
+//* Literal Characters
 re = /hello/;
 re = /hello/i;
 
-// Metacharacter Symbols
+//* Metacharacter Symbols
 re = /^h/i; // Must start with
 re = /d$/i; // Must end with
 re = /^hello$/i; // Must begin and end with
@@ -14,7 +14,7 @@ re = /gre?a?y/i; // Optional Character
 re = /gre?a?y\?/i; // Escape Character
 
 
-// Brackets [] - Characters Sets
+//* Brackets [] - Characters Sets
 re = /gr[ae]/i; // Must be an a or e
 re = /[GF]ray/i; // Must be an G or F
 re = /[^GF]ray/i; // Match anything except a G or F
@@ -23,20 +23,37 @@ re = /[a-z]ray/; // Match any lowercase letter
 re = /[A-Za-z]ray/; // Match any letter
 re = /[0-9]ray/; // Match any digit
 
-// Braces {} - Quantifiers
+//* Braces {} - Quantifiers
 re = /Hel{2}o/i; // Must occur exactly {m} amount of times
 re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times
 re = /Hel{2,}o/i; // Must occur at least {m} times
 
-// Parethesis () - Grouping
+//* Parethesis () - Grouping
 re = /^([0-9]x){3}$/ // Looking at the begining and the end
 
+// * Shorthand Character Classes
+re = /\w/; // Word character - alphanumeric or _
+re = /\w+/; // + = one or more
+re = /\W/; // Non-Word character
+re = /\d/; // Match any digit
+re = /\d+/; // Match any digit 0 or more times
+re = /\D/; // Match any Non-digit
+re = /\s/; // Match whitespace char
+re = /\S/; // Match non-whitespace char
+re = /Hell\b/i; // Word boundary
 
 
-// String to match
+// Assertions
+re = /x(?=y)/; // Match x only if followed by y
+re = /x(?!y)/; // Match x only if NOT followed by y
+
+
+
+
+// *String to match
 const str = '3x3x3x';
 
-// Log results
+// *Log results
 const result = re.exec(str);
 console.log(result);
 
